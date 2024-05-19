19c61dcf029a1e24233fcc0f85d243478d9c3a56
Francisco <97766435+MalaSuerte7@users.noreply.github.com>
2024-05-18 21:43:03 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import mysql.connector
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+#Permisos 
+origins = [
+    "*", #Todos los origenes 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Database connection information
 host_name = "database-1.crlz1rjtrz0e.us-east-1.rds.amazonaws.com"
@@ -13,11 +27,30 @@ database_name = "Train"
 
 # Estructura para representar un entrenador usando Pydantic
 class Trainer(BaseModel):
+    id: int
     nombre: str
-    apellido: str
-    medallas: int
-    fecha_nacimiento: str
-    edad: int
+    dinero: int
+# Estructura para representar un pokemons_de_entrenador usando Pydantic
+#     cursor.execute(f"INSERT INTO pokemons_de_entrenador (nombre_entrenador, nombre_pokemon, id_pokemon, id_entrenador, sprite, nivel, HP, tipo) VALUES ({nombre_entrenador},
+
+class pokemon_de_entrenador(BaseModel):
+    id: int
+    nombre_entrenador: str
+    nombre_pokemon: str
+    id_pokemon: int
+    id_entrenador: int
+    sprite: str
+    nivel: int
+    HP: int
+    tipo: str
+
+# objetos_de_entrenador
+class objetos_de_entrenador(BaseModel):
+    id: int
+    nombre_entrenador: str
+    id_entrenador: str
+    nombre_objeto: str
+    cantidad: int
 
 # Get all trainers
 @app.get("/trainers")
@@ -44,8 +77,8 @@ def get_trainer(id: int):
 def add_trainer(item: Trainer):
     mydb = mysql.connector.connect(host=host_name, port=port_number, user=user_name, password=password_db, database=database_name)
     cursor = mydb.cursor()
-    sql = "INSERT INTO entrenadores (nombre, apellido, medallas, fecha_nacimiento, edad) VALUES (%s, %s, %s, %s, %s)"
-    val = (item.nombre, item.apellido, item.medallas, item.fecha_nacimiento, item.edad)
+    sql = "INSERT INTO entrenadores (nombre, dinero) VALUES (%s, %s)"
+    val = (item.nombre, item.dinero)
     cursor.execute(sql, val)
     mydb.commit()
     mydb.close()
@@ -56,22 +89,12 @@ def add_trainer(item: Trainer):
 def update_trainer(id: int, item: Trainer):
     mydb = mysql.connector.connect(host=host_name, port=port_number, user=user_name, password=password_db, database=database_name)
     cursor = mydb.cursor()
-    sql = "UPDATE entrenadores SET nombre=%s, apellido=%s, medallas=%s, fecha_nacimiento=%s, edad=%s WHERE entrenador_id=%s"
-    val = (item.nombre, item.apellido, item.medallas, item.fecha_nacimiento, item.edad, id)
+    sql = "UPDATE entrenadores SET dinero=%s WHERE id=%s"
+    val = (item.dinero, id)
     cursor.execute(sql, val)
     mydb.commit()
     mydb.close()
     return {"message": "Trainer modified successfully"}
-
-# Delete a trainer by ID
-@app.delete("/trainers/{id}")
-def delete_trainer(id: int):
-    mydb = mysql.connector.connect(host=host_name, port=port_number, user=user_name, password=password_db, database=database_name)
-    cursor = mydb.cursor()
-    cursor.execute(f"DELETE FROM entrenadores WHERE entrenador_id = {id}")
-    mydb.commit()
-    mydb.close()
-    return {"message": "Trainer deleted successfully"}
 
 # Login
 @app.post("/login")
@@ -81,16 +104,15 @@ def login(username: str):
     cursor.execute(f"SELECT * FROM entrenadores WHERE nombre = '{username}'")
     result = cursor.fetchone()
     if result is None:
-        cursor.execute(f"INSERT INTO entrenadores (nombre, apellido, medallas, fecha_nacimiento, edad) VALUES ('{username}', 'Apellido', 0, '2000-01-01', 0)")
+        cursor.execute(f"INSERT INTO entrenadores (nombre, dinero) VALUES ('{username}', 1000)")
         cursor.execute(f"SELECT * FROM entrenadores WHERE nombre = '{username}'")
         result = cursor.fetchone()
-        cursor.execute(f"INSERT INTO inventarios (entrenador_id, pokebolas, pociones, dinero) VALUES ({result[0]}, 0, 0, 1000)")
         mydb.commit()
     mydb.close()
     return {"trainer": result}
 
 # Get Trainer pokemons
-@app.get("/pokemons_de_entrenador")
+@app.get("/pokemons_de_entrenador/{id_entrenador}")
 def get_pokemons_de_entrenador(id_entrenador: int):
     mydb = mysql.connector.connect(host=host_name, port=port_number, user=user_name, password=password_db, database=database_name)
     cursor = mydb.cursor()
@@ -101,16 +123,16 @@ def get_pokemons_de_entrenador(id_entrenador: int):
 
 # Create a new pokemon for a trainer
 @app.post("/crear_pokemon_de_entrenador")
-def crear_pokemon_de_entrenador(id_entrenador: int, id_pokemon: int, nivel: int, experiencia: int):
+def crear_pokemon_de_entrenador(nombre_entrenador: str, nombre_pokemon: str, id_pokemon: int, id_entrenador: int, sprite: str, nivel: int, HP: int, tipo: str):
     mydb = mysql.connector.connect(host=host_name, port=port_number, user=user_name, password=password_db, database=database_name)
     cursor = mydb.cursor()
-    cursor.execute(f"INSERT INTO pokemons_de_entrenador (id_entrenador, id_pokemon, nivel, experiencia) VALUES ({id_entrenador}, {id_pokemon}, {nivel}, {experiencia})")
+    cursor.execute(f"INSERT INTO pokemons_de_entrenador (nombre_entrenador, nombre_pokemon, id_pokemon, id_entrenador, sprite, nivel, HP, tipo) VALUES ('{nombre_entrenador}', '{nombre_pokemon}', {id_pokemon}, {id_entrenador}, '{sprite}', {nivel}, {HP}, '{tipo}')")
     mydb.commit()
     mydb.close()
     return {"message": "Pokemon created successfully"}
 
 # Show trainer inventory
-@app.get("/objetos_de_entrenador")
+@app.get("/objetos_de_entrenador/{id_entrenador}")
 def get_objetos_de_entrenador(id_entrenador: int):
     mydb = mysql.connector.connect(host=host_name, port=port_number, user=user_name, password=password_db, database=database_name)
     cursor = mydb.cursor()
@@ -121,20 +143,20 @@ def get_objetos_de_entrenador(id_entrenador: int):
 
 # Create a new object for a trainer
 @app.post("/crear_objeto_de_entrenador")
-def crear_objeto_de_entrenador(id_entrenador: int, id_objeto: int, cantidad: int):
+def crear_objeto_de_entrenador(id_entrenador: int, id_objeto: int, cantidad: int, nombre_objeto: str):
     mydb = mysql.connector.connect(host=host_name, port=port_number, user=user_name, password=password_db, database=database_name)
     cursor = mydb.cursor()
-    cursor.execute("INSERT INTO objetos_de_entrenador (id_entrenador, id_objeto, cantidad) VALUES ({id_entrenador}, {id_objeto}, {cantidad})")
+    cursor.execute(f"INSERT INTO objetos_de_entrenador (id_entrenador, id_objeto, cantidad, nombre_objeto) VALUES ({id_entrenador}, {id_objeto}, {cantidad}, '{nombre_objeto}')")
     mydb.commit()
     mydb.close()
     return {"message": "Object created successfully"}
 
 # Modify a trainer object
 @app.put("/modificar_objeto_de_entrenador")
-def modificar_objeto_de_entrenador(id_objeto: int, id_entrenador: int, id_objeto_nuevo: int, cantidad_nueva: int):
+def modificar_objeto_de_entrenador(id_entrenador: int, id_objeto: int, cantidad: int):
     mydb = mysql.connector.connect(host=host_name, port=port_number, user=user_name, password=password_db, database=database_name)
     cursor = mydb.cursor()
-    cursor.execute(f"UPDATE objetos_de_entrenador SET id_objeto={id_objeto_nuevo}, cantidad={cantidad_nueva} WHERE id_objeto={id_objeto} AND id_entrenador={id_entrenador}")
+    cursor.execute(f"UPDATE objetos_de_entrenador SET cantidad = {cantidad} WHERE id_entrenador = {id_entrenador} AND id_objeto = {id_objeto}")
     mydb.commit()
     mydb.close()
     return {"message": "Object modified successfully"}
